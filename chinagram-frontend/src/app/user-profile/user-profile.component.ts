bffa00161bc731c2ba4194c8877395916b664aee
Bukiatorrr <bukinator0272@gmail.com>
2019-08-02 04:29:18 +0400

    Попытки сделать макет профиля юзера

---
+++ chinagram-frontend/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../shared/api.service';
+import {User} from '../model/user';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+
+  user: User;
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+
+  }
+
+}
